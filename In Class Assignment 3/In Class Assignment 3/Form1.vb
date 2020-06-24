71e71aaf66b600da085e3f898c61c05f5691c3d3
NHQB <baon3183@gmail.com>
2020-06-24 16:55:16 -0400

    Handles clicking no button

--- In Class Assignment 3/In Class Assignment 3/Form1.vb
+++ In Class Assignment 3/In Class Assignment 3/Form1.vb
@@ -9,4 +9,24 @@ Public Class Form1
         MsgBox("Yes, you ARE awesome! Great job, and thank you.")
         Application.Exit()
     End Sub
+
+    ''' <summary>
+    '''  Handles clicking right no button
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub btnNoRight_Click(sender As Object, e As EventArgs) Handles btnNoRight.Click
+        btnNoRight.Visible = False
+        btnNoLeft.Visible = True
+    End Sub
+
+    ''' <summary>
+    '''  Handles clicking left no button
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub btnNoLeft_Click(sender As Object, e As EventArgs) Handles btnNoLeft.Click
+        btnNoLeft.Visible = False
+        btnNoRight.Visible = True
+    End Sub
 End Class
