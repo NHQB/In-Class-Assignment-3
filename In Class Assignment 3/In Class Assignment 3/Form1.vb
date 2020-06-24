a0ab9150a0450a06c1694d9eb9170377b826b90a
NHQB <baon3183@gmail.com>
2020-06-24 16:53:40 -0400

    Handles clicking yes button

--- In Class Assignment 3/In Class Assignment 3/Form1.vb
+++ In Class Assignment 3/In Class Assignment 3/Form1.vb
@@ -1,4 +1,12 @@
 ﻿Option Strict On
 Public Class Form1
-
+    ''' <summary>
+    ''' Handles clicking yes button
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub btnYes_Click(sender As Object, e As EventArgs) Handles btnYes.Click
+        MsgBox("Yes, you ARE awesome! Great job, and thank you.")
+        Application.Exit()
+    End Sub
 End Class
